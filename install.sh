6e052d956858f221360f9f285778bf2dc5e0cc55
Denis Kasak <denis.kasak@gmail.com>
2012-05-04 13:32:08 +0200

    Include pomodoro in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-binaries=(schedule tasks recreate_tasklist t/t.py)
+binaries=(pomodoro schedule tasks recreate_tasklist t/t.py)
 code_dir=~/code/pomodoro
 ln -r -s --force $code_dir/${binaries[@]} ~/bin/
