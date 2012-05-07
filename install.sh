f521c027c4dcadce2ad0115da44be84e5a215a14
Denis Kasak <denis.kasak@gmail.com>
2012-05-07 19:47:22 +0200

    Add distract to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-binaries=(pomodoro schedule tasks recreate_tasklist t/t.py)
+binaries=(pomodoro distract schedule tasks recreate_tasklist t/t.py)
 code_dir=~/code/pomodoro
-ln -r -s --force $code_dir/${binaries[@]} ~/bin/
+
+for file in ${binaries[@]}
+do
+    rm ~/bin/${file#*/} -f 
+    ln -r -s $code_dir/$file ~/bin/
+done
