197c300349f5589cbd66093400995c86ec6ab8ea
Denis Kasak <denis.kasak@gmail.com>
2012-05-09 22:35:59 +0200

    Implement configuration backend.

---
+++ pomodoro_config.py
@@ -0,0 +1,35 @@
+from os import sep
+from os.path import expanduser as expand
+
+settings = {}
+settings["SESSION_FILE"] = ".pomodoro_session"
+settings["SESSION_DIR"] = expand("~/.pomodoro/")
+
+settings["TASK_DIR"] = expand("~/.pomodoro/")
+settings["INVENTORY_FILE"] = "tasks"
+settings["SCHEDULE_FILE_PREFIX"] = "todo"
+settings["CURRENT_TASK_FILE"] = "current"
+
+try:
+    with open(expand("~/.pomodororc"), "r") as f:
+        user_settings = {}
+        for line in f:
+            setting, value = line.split('=')
+            setting = setting.strip()
+            value = value.strip()
+            # expand user directories in paths
+            if "DIR" in setting:
+                value = expand(value)
+                if not value.endswith('/'):
+                    value += '/'
+            user_settings[setting] = value
+            settings.update(user_settings)
+except IOError:
+    pass
+
+SESSION_DIR = settings["SESSION_DIR"]
+SESSION_FILE = settings["SESSION_FILE"]
+TASK_DIR = settings["TASK_DIR"]
+INVENTORY_FILE = settings["INVENTORY_FILE"]
+SCHEDULE_FILE_PREFIX = settings["SCHEDULE_FILE_PREFIX"]
+CURRENT_TASK_FILE = settings["CURRENT_TASK_FILE"]
